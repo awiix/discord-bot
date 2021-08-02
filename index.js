9a7cdb2f048b51c4bf165cc586e69cfb6f158b6d
awiix <88351736+awiix@users.noreply.github.com>
2021-08-03 00:51:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ client.on("ready", async () =>{
     client.user.setActivity("Face4Real sur YouTube", {type:'WatCHING'} );
 });
 
-client.login ("ODcxNDg2Nzk1MTAyMTY3MDkx.YQcBUw.vV4uF8P-HSOwWvXejq0m86PU1So")
+client.login ("ODcxNDg2Nzk1MTAyMTY3MDkx.YQcBUw.VuH_uMEePD9oNJVBRHcIcupDIN4")
